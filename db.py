6145edececd2f4da93b3e179f00feedad535e2a5
Marius Bongarts <mariusbongarts@web.de>
2021-04-18 22:17:08 +0200

    setup firebase-sdk json with secrets

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("firebase-sdk.json")
+cred = credentials.Certificate("firebase-sdk-prod.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
--- main.py
+++ main.py
@@ -14,18 +14,17 @@ def get_table(soup):
     return table
 
 def get_stock_info(href):
+    stock = {}
+
     soup = get_soup(base_url + href)
     instrument_id = soup.select('.instrument-id span[cptxt="WKN"]')[0].parent.text.split()
-    isin = instrument_id[len(instrument_id) - 1]
-    wkn = instrument_id[1]
+    stock["isin"] = instrument_id[len(instrument_id) - 1]
+    stock["wkn"] = instrument_id[1]
+    stock["id"] = stock["wkn"]
     price_target_container = soup.select('.iconTacho')[0].parent
-    price_target_percentage = price_target_container.select('strong')[0].text
-    price_target_absolute = price_target_container.select('strong')[1].text
-    stock = {}
-    stock["id"] = wkn
-    stock["href"] = href
-    stock["price_target_percentage"] = price_target_percentage
-    stock["price_target_absolute"] = price_target_absolute
+    stock["price_target_percentage"] = float(price_target_container.select('strong')[0].text.replace(',','.').replace('%',''))
+    stock["price_target_absolute"] = float(price_target_container.select('strong')[1].text.replace(',','.'))
+    stock["price"] = float(soup.select('.quotebox div')[0].text.split('EUR')[0].replace(',','.'))
     return stock
 
 table = get_table(get_soup(base_url + "/index/dax/30-werte"))
@@ -33,11 +32,11 @@ table = get_table(get_soup(base_url + "/index/dax/30-werte"))
 # Get different markets
 indexs = table.select('.box-nav li a')
 for index in indexs:
-    href = index.get('href')
-    title = index.text
-    print('############## ' + title + ' ##############')
+    index_href = index.get('href')
+    index_name = index.text
+    print('############## ' + index_name + ' ##############')
 
-    table = get_table(get_soup(base_url + href))
+    table = get_table(get_soup(base_url + index_href))
     rows = table.select('tr')
 
     for row in rows:
@@ -47,9 +46,12 @@ for index in indexs:
             a_element = None
             pass
         if a_element is not None:
+          try:
             href = a_element.get('href')
-            print(href)
             text = a_element.text
             stock = get_stock_info(href)
+            stock["index_name"] = index_name
+            stock["index_href"] = index_href
             write("stocks", stock)
-            print(stock)
+          except:
+            pass
