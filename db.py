a576e9628cc1efbdced6e799b6d1b7cf84e05294
Marius Bongarts <mariusbongarts@web.de>
2021-04-18 22:41:37 +0200

    read firebase config from json secret

--- db.py
+++ db.py
@@ -2,8 +2,20 @@ import firebase_admin
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
-cred = credentials.Certificate("firebase-sdk-prod.json")
+
+firebase_config = y = json.loads(os.environ['FIREBASE_SDK'])
+print(firebase_config)
+if firebase_config is not None:
+  with open('firebase-sdk-test.json', 'w') as f:
+    json.dump(firebase_config, f)
+
+
+cred = credentials.Certificate("firebase-sdk.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
